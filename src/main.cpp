d068c3bd64aac4c8a00ed372e94cd1b967646229
Michael Ward <michael24ward@gmail.com>
2020-03-14 16:47:37 +0000

    Colour sensor library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include "MimasV2Controller.h"
+#include "Adafruit_TCS34725.h"
 
 MimasV2Controller fpga = MimasV2Controller(12, 13, 10, 9);
+Adafruit_TCS34725 colourSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 void setup() {
   Serial.begin(9600);
